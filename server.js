11ec5f908b65568d8a8202cdceb0b0496a273fe6
Sagar Mondal <sagarandshiva@gmail.com>
2023-11-24 13:40:08 +0530

    Files are added

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const shortUrl = require('./models/shortUrl')
+const shortUrl = require('./models/shortUrl')
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.get('/', async (req, res) => {
+    const shortUrls = await shortUrl.find()
+    res.render('index', {shortUrls: shortUrls})
+})
+
+app.post('/shortUrls', async (req, res) => {
+    await shortUrl.create({full: req.body.fullUrl})
+    res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await shortUrl.findOne({ short: req.params.shortUrl })
+    if(shortUrl == null) return res.sendStatus(404)
+    shortUrl.clicks++
+    shortUrl.save()
+    res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 5000);
